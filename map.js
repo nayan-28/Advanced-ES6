6f94da9503f1b6b3d4094f4c511f506639bafbd8
nayan-28 <nayanmalakar720@gmail.com>
2024-01-31 00:18:10 +0600

    map

--- map.js
+++ map.js
@@ -1,9 +1,18 @@
 const numbers = [4, 5, 6, 7, 8, 10];
 
-const doubled = [];
-
-for (const num of numbers) {
-    const double = num * 2;
-    doubled.push(double);
+function doubleIt(num) {
+    return num * 2;
 }
-console.log(doubled);
+
+const result = numbers.map(doubleIt)
+
+console.log(result);
+
+
+// const doubled = [];
+
+// for (const num of numbers) {
+//     const double = num * 2;
+//     doubled.push(double);
+// }
+// console.log(doubled);
---
+++ more-map.js
@@ -0,0 +1,10 @@
+const numbers = [12, 10, 8, 15, 6];
+
+const doubled = numbers.map(num => num * 6);
+console.log(doubled);
+
+
+const friends = ['Nayan', 'Pervez', 'Refat', 'Sohag'];
+
+const nameLength = friends.map(friend => friend.length);
+console.log(nameLength);
